2cb503fdbb7df8e62433b253b6bfbbecf5b74f89
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2023-04-27 10:46:48 +0530

    Add files via upload

---
+++ slot-machine/slot-machine/Assets.xcassets/ContentView.dataset/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  slot-machine
+//
+//  Created by Harsh Rajput on 20/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slot-machine/slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Harsh Rajput on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
